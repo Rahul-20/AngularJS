948e339fe3a20c97bc886ee790a7978c8d5d1a51
Rahul Kumar Pamidi <Rahul@Example.com>
2017-04-03 22:33:20 +0530

    initial Commit

---
+++ WebContent/config/app.js
@@ -0,0 +1,33 @@
+var eventApp=angular.module("event",["ui.router"]);
+
+eventApp.controller("eventController", function($scope, $rootScope, $state){
+	alert('Entereeeeeeeeeeeeeddddddddd into ceventController');
+});
+
+eventApp.constant("URL",{});
+
+eventApp.config(function($stateProvider, $urlRouterProvider, $locationProvider){
+	$urlRouterProvider.otherwise(appPath + "/login");
+	$stateProvider
+	.state('login', {
+		cache: true,
+		url: appPath + "/login",
+		views: {
+			"header@":{templateUrl: appPath + "/Modules/Header/LoginHeader.tpl.html"},
+			
+			"@": {
+				templateUrl: appPath + "/Modules/Login/Login.tpl.html",
+				controller: ""
+			}
+		}		 
+	})
+});
+
+eventApp.run(function($state, $rootScope) {
+	$rootScope.$on('$stateChangeSuccess', function (e, toState, toParams, fromState, fromParams) {
+		if (toState.name == "login") {
+			$rootScope.moduleName = "login";
+		}
+	});	
+});
+
